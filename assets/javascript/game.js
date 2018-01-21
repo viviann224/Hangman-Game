468582f0915cec13d1e983b584b0674766bdbf31
viviann224 <viviann224@gmail.com>
2018-01-21 15:27:48 -0600

    cleaned up code with comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,9 @@
-//create a word array
-//sceret words
+/////////////////////////////////////////////
+//Vivian Tuong Nguyen
+//Javascript code for Hangman [Bulldog Moods]
+///////////////////////////////////////////////
+
+//create a word array of sceret words
 var arrayword = ["twins", "hungry", "doughnut", "longhorn", "confused", "snow", "tongue", "music", "yoda", "grumpy", "sleeping", "drink"];
 //creates an array of the secret array
 var arrayrand;
@@ -13,17 +17,13 @@ var counter=10;
 var isDone=false;
 //sets the wins to 0
 var wins=0;
-//var for user to guess
+//variable for user guess
 var userguess;
 
-//get array first choose a word randomly and creates dashes
+//getArray() first choose a word randomly and creates dashes
 //based on the length of the words
 //please look at the getArray() function to see the logic of the method.
 getArray();
-
-
-//debugging displays the secret word for debugging
-console.log(arrayrand);
 
 //gets user to guess when the user presses a key
 document.onkeyup = function()
@@ -34,27 +34,23 @@ document.onkeyup = function()
 	{
 		//gets the user input and converts the guess to lowercase
 		userguess = String.fromCharCode(event.keyCode).toLowerCase();
-		//debugging purpose to verify user compare to secret debugging
-console.log("userGuess: " +userguess);
-console.log("secret: " +arrayrand);
 
-
-		//calls fillArray() function and passes in the guess, the user'sarray, and the secret array
+		//calls fillArray() function and passes in the guess, user's array, and the secret array
 		//to update the user's blank array
 		document.getElementById("dash").innerHTML = fillArray(userguess, guessArray, arrayrand).join("");
 
-		//checks if the game is done
-		//checks the user's array and the secret array to see if matches
+		//isDone variable calls the compare() function to check if the game is done
+		//compare() function passes user's array and the secret array to see if matches
 		//if matches user get a point
 		//please look at the compare() function to see the logic of the method.
 		isDone=compare(guessArray, arrayrand);
 		//if the guess and secret array the same display winner, and give a point
+		//empty out the wrong array and call getArray() to start over
 		if(isDone&&counter>0)
 		{
 		document.getElementById("winner").innerHTML = "<br><br><h2>Congrats! YOU WIN!!</h2>";
 		wins++;
 		removeArray(wrongarray);
-		//wrongarray.pop();
 		getArray();
 		}
 		//if no more turns, tell user they lost and reset the game
@@ -72,7 +68,6 @@ console.log("secret: " +arrayrand);
 			//get a new word and blank array
 			getArray();
 		}
-
 		//displays how many guesses left for the user
 		document.getElementById("guessleft").innerHTML = "<br><br>Guesses Left: "+counter;
 		//displays the array of wrong letters
@@ -95,11 +90,10 @@ function getArray()
 	//makes the wrongarray empty for each use
 	wrongarray.length=0;
 	imgchange();
-
 }
 
-
-//function to split a string into an array of chars
+//passes in a word/string
+//function splits a string into an array of chars
 //returns the array of chars
 function charArray(userword)
 {
@@ -110,6 +104,7 @@ function charArray(userword)
 	}
 	return myArray;	
 }
+
 //passes secretword and create a blank array of dashes based
 //on the length of the secretword
 //returns the blank array of dashes
@@ -129,8 +124,7 @@ function blankArray(blankword)
 //fillArray method takes in a guess, the user's array, and the secretarray.
 //the function compares the guess to the secret array
 //if there is a match, update the guessarray
-//else but the guess in the wrong array and take a point away 
-//from the guesses.
+//else but the guess in the wrong array and take a point away from the guesses.
 //returns the updated guessarray
 function fillArray(guess, guessArray,secretArray)
 {
@@ -152,6 +146,7 @@ function fillArray(guess, guessArray,secretArray)
 	}
 	return guessArray;
 }
+
 //function removes the data inside of the wrongarray 
 //based on the length of the current array length
 function removeArray(wrongarray)
@@ -182,76 +177,57 @@ function compare(guessArray, secretArray)
 }
 
 //image changes depending on the secretArray
-//will look at the subindex of the secretArray and display the matching image associated to the secretArray
+//will look at the subindex of the secretArray and display the matching image
+// associated to the secretArray
 function imgchange()
 {
 	//the index of
 	if(arrayword.indexOf(arrayrand)==0)
 	{
-		console.log("inside the loop"+arrayword.indexOf(arrayrand))
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Are you seeing double?<br><img src='assets/images/hint1.png' alt='hint1' id='hint1'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Are you seeing double?<br><img src='assets/images/hint1.png' alt='hint1' id='hint1'>";
 	}
 	else if(arrayword.indexOf(arrayrand)==1)
 	{
-		console.log("inside the loop"+arrayword.indexOf(arrayrand))
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I love TacoDeli, if I'm not Hangry, I am.. what?<br><img src='assets/images/hint2.png' alt='hint2' id='hint2'>"
-
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I love TacoDeli, if I'm not Hangry, I am.. what?<br><img src='assets/images/hint2.png' alt='hint2' id='hint2'>";
 	}
 	else if(arrayword.indexOf(arrayrand)==2)
 	{
-		console.log("inside the loop"+arrayword.indexOf(arrayrand))
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> My human says this is a google mini and not a.. what?<br><img src='assets/images/hint3.png' alt='hint3' id='hint3'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> My human says this is a google mini and not a.. what?<br><img src='assets/images/hint3.png' alt='hint3' id='hint3'>";
 	}
 	else if(arrayword.indexOf(arrayrand)==3)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> What am I if I bleed burnt orange?<br><img src='assets/images/hint4.png' alt='hint4' id='hint4'>"
-		//console.log("inside the loop for index 3"+arrayword.indexOf(arrayrand))
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> What am I if I bleed burnt orange?<br><img src='assets/images/hint4.png' alt='hint4' id='hint4'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==4)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I'm what..? Some people call it deer stuck in the headlights.<br><img src='assets/images/hint5.png' alt='hint5' id='hint5'>"
-		//console.log("inside the loop for index4"+arrayword.indexOf(arrayrand))
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I'm what..? Some people call it deer stuck in the headlights.<br><img src='assets/images/hint5.png' alt='hint5' id='hint5'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==5)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> What is in this picture that rarely happens in Austin?<br><img src='assets/images/hint6.png' alt='hint6' id='hint6'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> What is in this picture that rarely happens in Austin?<br><img src='assets/images/hint6.png' alt='hint6' id='hint6'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==6)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I LOVE TOT, but you can barely see it<br><img src='assets/images/hint7.png' alt='hint7' id='hint7'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I LOVE TOT, but you can barely see it<br><img src='assets/images/hint7.png' alt='hint7' id='hint7'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==7)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Austin is the capital for what?<br><img src='assets/images/hint8.png' alt='hint8' id='hint8'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Austin is the capital for what?<br><img src='assets/images/hint8.png' alt='hint8' id='hint8'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==8)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Do you like my Halloween costume? <br>Guess who I am (may the force be with you)?<br><img src='assets/images/hint9.png' alt='hint9' id='hint9'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Do you like my Halloween costume? <br>Guess who I am (may the force be with you)?<br><img src='assets/images/hint9.png' alt='hint9' id='hint9'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==9)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Guess my mood? There is a cat I would love to meet!<br><img src='assets/images/hint10.png' alt='hint10' id='hint10'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> Guess my mood? There is a cat I would love to meet!<br><img src='assets/images/hint10.png' alt='hint10' id='hint10'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==10)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> If I am not awake, then what am I doing?<br><img src='assets/images/hint11.png' alt='hint11' id='hint11'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> If I am not awake, then what am I doing?<br><img src='assets/images/hint11.png' alt='hint11' id='hint11'>";
 	}
 	else if (arrayword.indexOf(arrayrand)==11)
 	{
-		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I want to play, but what is my mom holding?<br><img src='assets/images/hint12.png' alt='hint12' id='hint12'>"
-
+		document.getElementById("hint").innerHTML="<h3>HINT:</h3> I want to play, but what is my mom holding?<br><img src='assets/images/hint12.png' alt='hint12' id='hint12'>";
 	}
 }
-
-
-
